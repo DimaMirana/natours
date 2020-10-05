7fee311d95525c1cb105df429a0e4c6c6187edbc
Tasnim Dima <tasnimdima25@gmail.com>
2020-10-05 23:51:26 +0600

    agreegation midleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -94,8 +94,15 @@ tourSchema.pre(/^find/,function(next){
     next();
 });
 tourSchema.post(/^find/,function(docs,next){
-    console.log(`Query took ${Date.now() - this.start} milliseconds`)
-    console.log(docs);
+    // console.log(`Query took ${Date.now() - this.start} milliseconds`)
+    // console.log(docs);
+    next();
+})
+
+//AGGREGATION MIDDLEWARES
+tourSchema.pre('aggregate',function(next){
+    this.pipeline().unshift({$match: {secretTour: {$ne:true}} });
+    console.log(this.pipeline())
     next();
 })
 
